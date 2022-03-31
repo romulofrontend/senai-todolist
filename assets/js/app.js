66c26b6d4c4a96766927fa17bf90f0ef5ae1d3a2
Romulo Rosa <romulo.frontend@gmail.com>
2022-03-30 22:17:32 -0300

    funcionalidades

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,28 @@
-const btnAddToDo = document.querySelector("#btn-add-todo");
+function getItem(item){
+  return document.querySelector(item);
+}
+
+const btnAddToDo = getItem("#btn-add-todo");
 
 btnAddToDo.addEventListener("click", (evento) => {
-  const inputToDo = document.querySelector("#input-todo");
+  const inputToDo = getItem("#input-todo");
   const inputValue = inputToDo.value;
   console.log(inputValue);
+
+  if(inputValue = ""){
+    console.log("String vazia");
+  }else{
+    const todoList = getItem("#todo-list");
+
+    const listItem = document.createElement("li");
+    const textItem = document.createTextNode(inputValue);
+
+    listItem.appendChild(textItem);
+    listItem.classList.add("list-group-item");
+
+    todoList.appendChild(listItem);
+    inputToDo = "";
+
+  }
+
 });
