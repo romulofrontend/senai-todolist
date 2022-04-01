1270e75ec2339ee6c7f36f85f0cc29c156f5e6af
Romulo Rosa <romulo.frontend@gmail.com>
2022-03-31 21:20:09 -0300

    adicionando evento com enter

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,27 +2,55 @@ function getItem(item){
   return document.querySelector(item);
 }
 
-const btnAddToDo = getItem("#btn-add-todo");
 
-btnAddToDo.addEventListener("click", (evento) => {
-  const inputToDo = getItem("#input-todo");
-  const inputValue = inputToDo.value;
-  console.log(inputValue);
+function addToDo(todoText){
+  const div = document.createElement("div");
+    div.classList.add("form-check");
 
-  if(inputValue = ""){
-    console.log("String vazia");
-  }else{
-    const todoList = getItem("#todo-list");
+    const input = document.createElement("input");
+    input.type = "checkbox";
+    input.classList.add("form-check-input");
+
+    const label = document.createElement("label");
+    label.classList.add("form-check-label");
+
+    const textItem = document.createTextNode(todoText);
+    
+    label.appendChild(textItem);
+    div.appendChild(input);
+    div.appendChild(label);
+
 
     const listItem = document.createElement("li");
-    const textItem = document.createTextNode(inputValue);
-
-    listItem.appendChild(textItem);
+    listItem.appendChild(div);
     listItem.classList.add("list-group-item");
 
+    const todoList = getItem("#todo-list");
     todoList.appendChild(listItem);
-    inputToDo = "";
+}
 
+const inputToDo = getItem("#input-todo");
+
+inputToDo.addEventListener("keydown", (evento) => {
+  if(evento.code == "Enter"){
+    console.log("Enter pressionado");
+    const inputValue = inputToDo.value;
+    addToDo(inputValue);
+    inputToDo.value = "";
+  }
+});
+
+const btnAddToDo = getItem("#btn-add-todo");
+btnAddToDo.addEventListener("click", (evento) => {
+  
+  const inputValue = inputToDo.value;
+
+  console.log(inputValue);
+  if(inputValue == ""){
+    console.log("String vazia");
+  }else{
+    addToDo(inputValue);    
+    inputToDo = "";
   }
 
 });
